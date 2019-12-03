18d54ceb4a926a4e75a40b14e62b43f01cef01af
tylerdav <tdavis1433@att.net>
2019-12-03 14:23:43 -0600

    other things

--- scripts/france/cities/city.js
+++ scripts/france/cities/city.js
@@ -1,13 +1,6 @@
 const CityComponent = city => {
   return `
-
-    <dialog>
-
-      <div>${city.name}</div>
-
-
-    <button class="button--close">Close Dialog</button>
-    </dialog>
+  <li>${city.name}</li>
   `;
 };
 
