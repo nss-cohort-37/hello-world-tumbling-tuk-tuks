e102ffb7153c57ba31d0632ca7f686e3bbe42eff
DanielBradleyBennett <danielbradleybennett@gmail.com>
2019-12-03 14:28:01 -0600

    dialogueevent

--- scripts/scotland/cities/City.js
+++ scripts/scotland/cities/City.js
@@ -1,7 +1,21 @@
 const CityComponent = (city) => {
   return ` 
     
-      <li>${city.name}</li>
+      <div>${city.name}</div>
+      <div class="city">
+          
+          <button id="button--${city.name}">Details</button>
+
+          <dialog class="dialog--fish" id="details--${fish.name}">
+              <div> ${fish.img}</div>
+              <div>population: ${fish.location}</div>
+              <div>Length: ${fish.length}</div>
+              <div>Food: ${fish.food.join(",")}</div>
+
+              <button class="button--close">Close Dialog</button>
+          </dialog>
+      </div>
+  `
 
 
 
